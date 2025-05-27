da5a3a34b0725559fc01deb525d0392d6e55634a
Vimal Rajput <vimal.rajput.v11@gmail.com>
2025-05-28 00:46:46 +0530

    Updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -85,7 +85,7 @@ const Navbar = () => {
                 <NavLink to='/' onClick={()=>setOpen(false)}>Home</NavLink>
                 <NavLink to='/products' onClick={()=>setOpen(false)}>All Products</NavLink>
                 {user &&
-                <NavLink to='/products' onClick={()=>setOpen(false)}>My Orders</NavLink>
+                <NavLink to='/my-orders' onClick={()=>setOpen(false)}>My Orders</NavLink>
                 }
                 <NavLink to='/' onClick={()=>setOpen(false)}>Contact</NavLink>
 
